009feffcc2147ff16ded6a2f35e60345a1cc781f
Estefani Keiko Fujimoto <estefani.fujimoto@psicologiaviva.com.br>
2023-06-20 23:19:42 -0300

    feat | Init

---
+++ feed-typescript/src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sibebar";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/pv-estefanifujimoto.png",
+      name: "Estefani K. Fujimoto",
+      role: "Front End Developer @conexa_saude",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2023-06-20 16:44:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/RPlima.png",
+      name: "Raphael de Lima",
+      role: "Full Stack Developer @rh.rita",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2023-05-10 10:15:00"),
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </div>
+  );
+}
