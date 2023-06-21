6a4b6829512d8d3857cbe7306c7ad867fa2d706e
ekfuji <estefani.fujimoto@hotmail.com>
2023-06-21 08:26:02 -0300

    feat | adapting project that was in javascript to typescript

---
+++ feed-typescript/src/components/Comment.tsx
@@ -0,0 +1,62 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import { useState } from "react";
+import { Avatar } from "./Avatar";
+import styles from "./Comment.module.css";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeCount() {
+    // setLikeCount(likeCount + 1);
+    // Sempre que tivermos que atualizar uma informação e houver dependência do valor
+    // que ela tinha anteriormente, devemos usar o padrão de funções citado abaixo:
+    setLikeCount((stateLikeCount) => {
+      return stateLikeCount + 1;
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/pv-estefanifujimoto.png"
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Um certo alguém</strong>
+              <time title="17 de abril às 21h32" dateTime="2023-04-17 21:32">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button
+              onClick={handleDeleteComment}
+              onFocus={handleDeleteComment}
+              title="Deletar comentário"
+            >
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeCount}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
